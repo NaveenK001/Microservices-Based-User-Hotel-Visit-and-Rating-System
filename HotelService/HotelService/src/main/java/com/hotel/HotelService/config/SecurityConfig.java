450398361326927ea47a0e9960d9d7ca1820c690
Naveen <115355331+NaveenK001@users.noreply.github.com>
2024-11-18 18:03:23 +0530

    Add files via upload

---
+++ HotelService/HotelService/src/main/java/com/hotel/HotelService/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.hotel.HotelService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true) // Replacing @EnableGlobalMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults()) // Updated method
+                );
+
+        return http.build();
+    }
+}
+
